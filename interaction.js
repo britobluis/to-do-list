13aa6c8fe5e1f745bc6041f4d80bb6b062d52a6e
Luis Brito <nottluis@gmail.com>
2019-08-29 16:01:25 -0300

    Improved styles, added checked functionality to list items

--- interaction.js
+++ interaction.js
@@ -2,8 +2,8 @@ $(function() {
   var listItems = function() {
     var val = $("input").val();
     if(val !== "") {
-       var elem = $("<li></li>").text(val);
-      $(elem).append("<button class='rem'>X</button>");
+      var elem = $("<li></li>").text(val);
+      $(elem).append("<span class='rem'>X</span>");
       $(".mylist").append(elem);
       $("input").val("");
       $(".rem").on('click', function(){
@@ -15,4 +15,12 @@ $(function() {
   $("input").keypress(function(event) {
     if (event.keyCode == 13) listItems();
   });
+  // Add a "checked" symbol when clicking on a list item
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+    }
+  }, false);
+
 });
